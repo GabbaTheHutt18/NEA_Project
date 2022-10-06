de06c427039d51c85c700811c808b45690dafe5f
Gabriella Emerson <2016045@folkestonegirls.kent.sch.uk>
2022-10-06 09:08:43 +0100

    Add project files.

---
+++ Helpers/SimpleCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace NEA_Project.Helpers
+{
+    public class SimpleCommand : SimpleCommand<object>
+    {
+        public SimpleCommand(Action<object> action) : base(action)
+        {
+
+        }
+    }
+
+    public class SimpleCommand<T> : ICommand where T : class
+    {
+        private readonly Action<T> _action;
+
+        public SimpleCommand(Action<T> action)
+        {
+            _action = action;
+            CanExecuteChanged = (sender, args) => { };
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action.Invoke((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+    }
+}
